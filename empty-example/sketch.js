cb6a208c51789d2f2d20b6ccae5085d0b57cb093
SufiAzarquiel <alumno.745709@ies-azarquiel.es>
2022-10-24 10:12:16 +0200

    Falling rect
    Draw a rectangle with gravity and move it with arrow keys.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,18 +1,45 @@
 const WIDTH = 640;
 const HEIGHT = 640;
 
+const cX = WIDTH / 2;
+const cY = HEIGHT / 2;
+const VEL = 10;
+var myRect = { x: cX, y: cY, w: 30, h: 30};
+
 function setup() {
   // put setup code here
   createCanvas(WIDTH, HEIGHT);
+  background(220);
+  fill(0, 0, 0);
 }
 
 function draw() {
   // put drawing code here
-  background(220);
-  if (mouseIsPressed) {
-    fill(0, 0, 139);
-  } else {
-    fill(0, 255, 0);
+  if (keyIsDown(LEFT_ARROW)) {
+    myRect.x -= VEL;
   }
-  ellipse(mouseX, mouseY, 80, 80);
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    myRect.x += VEL;
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    myRect.y -= VEL;
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    myRect.y += VEL;
+  }
+
+  if(myRect.y + myRect.h < HEIGHT) {
+    if (keyIsDown(UP_ARROW)) {
+      myRect.y -= VEL;
+    } else {
+      myRect.y += VEL;
+    }
+  }
+
+  background(220);
+  rect(myRect.x, myRect.y, myRect.w, myRect.h);
+  describe('Move square up and down with arrow keys.');
 }
