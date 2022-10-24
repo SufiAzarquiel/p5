51ede42c059cf1c84f46bb0849782bd2b9b012f7
SufiAzarquiel <alumno.745709@ies-azarquiel.es>
2022-10-24 09:31:25 +0200

    P5 Starting point
    Added a simple p5 script and updated readme file.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,18 @@
+const WIDTH = 640;
+const HEIGHT = 640;
+
 function setup() {
   // put setup code here
+  createCanvas(WIDTH, HEIGHT);
 }
 
 function draw() {
   // put drawing code here
+  background(220);
+  if (mouseIsPressed) {
+    fill(0, 0, 139);
+  } else {
+    fill(0, 255, 0);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
