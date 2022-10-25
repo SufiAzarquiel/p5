bf9eab8cdc90c9fda02327d46d48d69c21e572b8
SufiAzarquiel <alumno.745709@ies-azarquiel.es>
2022-10-25 10:28:41 +0200

    Update game.js
    Add dirt pixels and change ground.

--- game.js
+++ game.js
@@ -12,7 +12,8 @@ let cloudImg;
 let terrain_1;
 
 let ground;
-const bgColor = 0;
+let dirtDensity = 10;
+const bgColor = 255;
 let quit = false;
 
 function preload() {
@@ -39,26 +40,38 @@ function setup() {
   background(bgColor);
 
   ground = new Sprite();
-  ground.y = 500;
+  ground.y = height - 100;
   ground.x = width / 2;
-  ground.w = height;
-  ground.h = 10;
+  ground.w = width;
+  ground.h = 3;
   ground.collider = "static";
+  ground.color = "black";
 
   clouds = new Group();
   clouds.w = 110;
   clouds.h = 50;
-  clouds.x = () => random(0, 600);
-  clouds.y = () => random(50, 250);
+  clouds.x = () => random(0, width);
+  clouds.y = () => random(100, 400);
   clouds.addImage("cloud", cloudImg);
-  clouds.amount = 3;
+  clouds.amount = dirtDensity;
   clouds.collider = "none";
+  clouds.vel.x = -1;
+
+  dirt = new Group();
+  dirt.color = "black";
+  dirt.w = () => random(6, 20);
+  dirt.h = 5;
+  dirt.x = () => random(0, width/dirtDensity) * dirtDensity;
+  dirt.y = () => ground.y + (random(0, 5)*dirt.h*2);
+  dirt.amount = dirtDensity;
+  dirt.collider = "none";
+  dirt.vel.x = -5;
 
   dino = new Sprite();
   dino.x = 200;
   dino.width = 70;
-  dino.height = 101;
-  runAni.frameDelay = 50;
+  dino.height = 102;
+  runAni.frameDelay = 10;
   dino.addAni("run", runAni);
 }
 
@@ -69,21 +82,35 @@ function draw() {
 
   if (kb.presses("p")) {
     quit = true;
+    runAni.stop();
+    clouds.vel.x = 0;
+    dirt.vel.x = 0;
   } else if (kb.presses("s")) {
     quit = false;
+    runAni.play();
+    clouds.vel.x = -2;
+    dirt.vel.x = -5;
   }
 
   if (!quit) {
     // Handle player movement
     movePlayer();
 
+    dino.rotation = 0;
+
     // Handle background movement
     for (let i = 0; i < 3; i++) {
-      if (clouds[i].x < -100){
-        clouds[i].x = random(550, 650);
+      if (clouds[i].x < -100) {
+        clouds[i].x = random(width, width+100);
         clouds[i].y = random(50, 250);
+        clouds.vel.x = -2;
       }
-      else clouds[i].x -= 5;
+    }
+    for (let i = 0; i < dirtDensity; i++) {
+      if (dirt[i].x < -100) {
+        dirt[i].x = width;
+        dirt.vel.x = -5;
+      }
     }
     clouds.debug = mouse.holding();
     dino.debug = mouse.holding();
@@ -94,6 +121,7 @@ function draw() {
 function movePlayer() {
   // Change velocity based on key pressed
   if (kb.presses("ArrowUp")) {
-    dino.vel.y = -6;
+      dino.vel.y = -6;
+      subsequent = false;
   }
 }
