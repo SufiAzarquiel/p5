614299ac37f8156918f1feaad51c77a049b21537
SufiAzarquiel <48214093+SufiDev@users.noreply.github.com>
2022-11-14 21:53:44 +0100

    Update player.js

--- player.js
+++ player.js
@@ -10,39 +10,35 @@ class Player {
         this.sprite.height = playerSize;
         // this.sprite.x = width / 2 - playerSize;
         // this.sprite.y = height / 2 - playerSize;
-        this.sprite.x = playerSize*2;
+        this.sprite.x = playerSize * 2;
         this.sprite.y = WINDOWSIZE - playerSize;
         // this.sprite.addAni(runAni);
         // this.sprite.ani.frameDelay = 20;
         this.sprite.rotation = 0;
         this.sprite.collider = "none";
-        this.state = "runing";
+        this.state = "running";
     }
 
     // Handle player movement
     move() {
+        // Change state based on input and state
         if (kb.presses("ArrowUp")) {
-            if(state != "jumping"){
-                this.sprite.vel.y -= 5;
-                state = "jumping";
-            } else {
-                state = "runing";
+            if (this.state != "jumping") {
+                this.state = "jumping";
             }
-        } else if (kb.pressing("ArrowDown")) {
-           if (state == "jumping") {
-                this.sprite.vel.y += 5;
-                state = "diving"
-           }
+        }
+
+        if (this.state == "jumping") {
+            this.sprite.vel.y = -10;
         } else {
-            if (state = "diving") {
+            if (this.sprite.y < ) {
                 
             }
-            this.sprite.vel.y = 0;
         }
     }
 
     // Pause all movement and rotation
-    static(){
+    static() {
         this.sprite.vel.x = 0;
         this.sprite.vel.y = 0;
         this.sprite.rotation = 0;
